90a6a7ce0ac3697600cc11a654cc6c488b235477
DaTerryMan <jacgalla@flemingcollege.ca>
2024-01-24 15:15:21 -0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -34,7 +34,8 @@ def print_student_name_and_id(about_me):
     
 # TODO: Step 5 - Function that adds pizza toppings to data structure
 def add_pizza_toppings(about_me, toppings):
-    
+    about_me['pizza_toppings'].append({'pineapple',
+                                      'cheese'})
     return
 
 # TODO: Step 6 - Function that prints bullet list of pizza toppings
